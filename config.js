8381a1aef53325eb873550a7a193ddfa084eca1c
Spacioam <jforce15@gmail.com>
2026-09-25 03:17:58 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Pega aquí el Client ID de OAuth de Google Cloud (tipo "Aplicación web").
 // Mientras esté vacío, la app funciona en modo demostración.
 window.GSP_CONFIG = {
-  clientId: '',
+  clientId: '946315464909-i0uptgcvbpqnspnjp5a0o4a7dnpi0ngi.apps.googleusercontent.com',
 };
