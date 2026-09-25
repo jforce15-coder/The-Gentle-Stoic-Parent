99427f8455be0e60695548e0722f669a02e738ae
Spacioam <jforce15@gmail.com>
2026-09-25 12:11:05 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 window.GSP_CONFIG = {
   // Client ID de OAuth (Google Cloud → Credenciales)
   clientId: '946315464909-i0uptgcvbpqnspnjp5a0o4a7dnpi0ngi.apps.googleusercontent.com',
-  // URL de la aplicación web del Apps Script (termina en /exec). Vacío = sin correos automáticos.
-  appsScriptUrl: '',
+  // Aplicación web del Apps Script (correos automáticos, familia, contraseñas)
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbzlD8c0g_6lHZAQCR8LK1nYBT_OuOkmMlPIf_2nxqQqNlQKDuFlMtK8mFAuA7mYaxpnHg/exec',
 };
